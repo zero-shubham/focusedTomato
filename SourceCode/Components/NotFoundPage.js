e274e09eb6542e7d5bfce19bdf883858bdbe66b3
Shubham Biswas <shubhambiswas_zero@yahoo.com>
2019-04-22 18:54:58 +0530

    resting all

--- SourceCode/Components/NotFoundPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-const NotFoundPage = () => (
-    <div>
-        404! <Link to='/'>Go Home</Link>
-    </div>
-);
-export default NotFoundPage;
